3d02c936a3d350faa9715880071ec88f4a7ef9db
manthanjindal <manthanjindal08@gmail.com>
2026-06-29 16:57:11 +0530

    trying to add some more features backing up before thigns go south

--- pokeapi.py
+++ pokeapi.py
@@ -3,29 +3,32 @@ import time
 
 base_url = 'https://pokeapi.co/api/v2/'
 
-def get_pokemon_data(url):
-    url = f'{base_url}pokemon/{pokemon_name}'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        pokemon_data = response.json()
-
+def print_delay():
         print("retriving data...")
-
         time.sleep(0.5)
-        print('...')
+        print(' . ', end = '')
         time.sleep(0.5)
         print('...')
         time.sleep(0.5)
         print('...') 
         time.sleep(0.5)
 
+def get_pokemon_data(url):
+    url = f'{base_url}pokemon/{pokemon_name}'
+    response = requests.get(url)
 
-        print(pokemon_data)       
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return(pokemon_data)       
 
     else:
         print(f"Failed to retrive data {response.status_code}")
 
 pokemon_name = 'pikachu'
-get_pokemon_data(pokemon_name)
+pokemon_info = get_pokemon_data(pokemon_name)
 
+
+if pokemon_info:
+    print(f'{pokemon_info["name"]}')
+    print(f'{pokemon_info["id"]}')
+    print(f'{pokemon_info["height"]}')
